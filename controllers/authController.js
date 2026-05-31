9f1187e8b379a62f998218411493d2de7e59107c
dssff <dendssff@gmail.com>
2026-05-31 19:03:44 +0300

    Lab 10

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+
 
 exports.register = async (req, res) => {
     try {
@@ -48,4 +50,81 @@ exports.register = async (req, res) => {
     } catch (err) {
         res.status(500).json({ success: false, message: err.message });
     }
+};
+const generateToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+
+exports.login = async (req, res) => {
+
+  try {
+    const { email, password } = req.body;
+
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: 'Введіть email та пароль'
+      });
+    }
+
+
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'Невірний email або пароль'
+      });
+
+    }
+
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: 'Невірний email або пароль'
+      });
+    }
+
+    const token = generateToken(user._id, user.role);
+
+
+    res.status(200).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+
+  } catch (err) {
+
+    res.status(500).json({ success: false, message: err.message });
+
+  }
+
+};
+
+exports.getMe = async (req, res) => {
+
+  try {
+
+
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+      success: true,
+      user
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 };
